3e31fe4dae7b75386a5c56114d0054aec3762112
tcyang <tcyang@gm.pu.edu.tw>
2021-06-06 16:56:56 +0800

    v3新增修改

--- app/src/main/java/tw/edu/pu/csim/tcyang/testf1/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/testf1/MainActivity.kt
@@ -2,6 +2,8 @@ package tw.edu.pu.csim.tcyang.testf1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,5 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnUpdate.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                user["名字"] = edtName.text.toString()
+                user["出生體重"] = edtWeight.text.toString().toInt()
+                db.collection("Users")
+                        //.document(edtName.text.toString())
+                        //.set(user)
+                        .add(user)
+                        .addOnSuccessListener {
+                            Toast.makeText(baseContext, "新增/異動資料成功",
+                                    Toast.LENGTH_LONG).show()
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(baseContext, "新增/異動資料失敗：" + e.toString(),
+                                    Toast.LENGTH_LONG).show()
+                        }
+            }
+        })
+
     }
 }
