238138fe6a2c6f9ad0da8d4c553bc47bd4ba152f
tcyang <tcyang@gm.pu.edu.tw>
2021-06-06 16:35:36 +0800

    v2c初始化設定Firestore

--- app/src/main/java/tw/edu/pu/csim/tcyang/testf1/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/testf1/MainActivity.kt
@@ -2,9 +2,14 @@ package tw.edu.pu.csim.tcyang.testf1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var db = FirebaseFirestore.getInstance()
+    var user: MutableMap<String, Any> = HashMap()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
